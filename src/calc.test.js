3bfa73bff1643dcca02ecc2fe6855e5c1d958c31
Sean Trevor <sean.trevor@cgi.com>
2022-02-01 11:25:45 -0500

    Updated calc with subtract and added subtract test

--- src/calc.test.js
+++ src/calc.test.js
@@ -1,6 +1,17 @@
 import { TestWatcher } from "jest";
-import { sum } from "./calc";
+import { sum, subtract } from "./calc";
+
+beforeEach(() => {
+    console.log("To be printed before each test: Test Beginning now!");
+})
+afterEach(() => {
+    console.log("To be printed after each test: Test Ended!");
+})
 
 test("Adding 2 + 3 should give me 5", () => {
     expect(sum(2,3)).toBe(5);
+})
+
+test("Subtract 2 - 3 should give me -1", () => {
+    expect(subtract(2,3)).toBe(-1);
 })
